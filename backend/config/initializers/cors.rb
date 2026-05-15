98686dd2b2d83cb967f0e99b8aa438b68381f819
THE-MAIN39 <youremail@example.com>
2026-05-15 15:15:24 +0500

    Update CORS origins for Vercel deployment

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
+    # Add your Vercel URL here!
+    origins 'https://ecommerce-fullstack-design-woad.vercel.app', 'http://localhost:5173'
+
     resource '*',
       headers: :any,
-      expose: ['Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
   end
 end
